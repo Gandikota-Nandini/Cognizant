b9c94d3d6a2b0e455bb0d4ebc5c33b74c91f49ce
Gandikota-Nandini <nandinigandikota2006@gmail.com>
2026-05-30 11:16:02 +0530

    modified

--- SQl/Queries/7.sql
+++ SQl/Queries/7.sql
@@ -2,4 +2,4 @@ SELECT u.full_name, f.comments, e.title AS event_name
 FROM Feedback f
 JOIN Users u ON f.user_id = u.user_id
 JOIN Events e ON f.event_id = e.event_id
-WHERE f.rating < 4;
+WHERE f.rating < 3;
